101d119b69f2dea1082f73b350f8338bd78b6a3b
jarekratajski <jratajski@gmail.com>
2018-06-13 09:35:38 +0200

    begin of fibonacci

--- src/main/kotlin/pl/setblack/fibo/FiboServer.kt
+++ src/main/kotlin/pl/setblack/fibo/FiboServer.kt
@@ -18,22 +18,13 @@ class FiboServer {
             GET("/fib/{n}")
             { request ->
                 val n = Integer.parseInt(request.pathVariable("n"))
-                if (n < 2) {
-                    ServerResponse.ok().contentType(MediaType.TEXT_HTML).body(fromObject<String>(n.toString()))
-                } else {
-                    val n_1 = WebClient.create("http://localhost:8080").get().uri("/fib/{n}", n - 1)
+                    /*val n_1 = WebClient.create("http://localhost:8080").get().uri("/fib/{n}", n - 1)
                             .accept(MediaType.TEXT_HTML).exchange()
                             .flatMap { resp -> resp.bodyToMono(String::class.java) }
                             .map<Int>{ Integer.parseInt(it) }
-                    val n_2 = WebClient.create("http://localhost:8080").get().uri("/fib/{n}", n - 2)
-                            .accept(MediaType.TEXT_HTML).exchange().flatMap { resp -> resp.bodyToMono(String::class.java) }
-                            .map<Int> { Integer.parseInt(it) }
-
-                    val result = n_1
-                            .flatMap { a -> n_2.map { b -> a!! + b!! } }
-                            .map<String>( { it.toString() })
-                    ServerResponse.ok().contentType(MediaType.TEXT_HTML).body(fromPublisher<String, Mono<String>>(result, String::class.java))
-                }
+                    */
+                    //ServerResponse.ok().contentType(MediaType.TEXT_HTML).body(fromPublisher<String, Mono<String>>(result, String::class.java))
+                ServerResponse.ok().contentType(MediaType.TEXT_HTML).body(fromObject("Hello world!"))
             }
         }
 
@@ -46,7 +37,5 @@ class FiboServer {
 
 
 fun main(args: Array<String>) {
-    println("Hello, world!")
     FiboServer().start()
-
 }
